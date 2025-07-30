7e1baf19f15609ae6d46a14d12448db72516c9d8
Andrehlb <andreh.72@hotmail.com>
2025-07-30 19:31:29 -0300

    Add TvShow class for TV show data model

---
+++ flutter_app2_series/lib/tv-show_model.dart
@@ -0,0 +1,13 @@
+class TvShow {
+  final String title;
+  final String stream;
+  final String rating;
+  final String summary;
+
+  TvShow({
+    required this.title,
+    required this.stream,
+    required this.rating,
+    required this.summary,
+  });
+}
